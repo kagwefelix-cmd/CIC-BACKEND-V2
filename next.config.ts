6ae560233242a6b6e4865a0ffca488b79d511e9d
kagwefelix-cmd <kagwe.felix@gmail.com>
2026-05-10 20:10:28 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,21 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'chatterinnercircle.vercel.app',
+          },
+        ],
+        destination: 'https://chattersinnercircle.vercel.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
